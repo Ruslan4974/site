49ca41435f5172161ff4db0cf87d93dd3ace19cf
Ruslan <36664273+Ruslan4974@users.noreply.github.com>
2021-02-22 22:21:20 +0400

    Add files via upload

---
+++ semestr prо/main1.js
@@ -0,0 +1,406 @@
+window.onload = function(){
+	var MODAL_OVERLAY_CLASS_NAME2 = 'modal-overlay2';
+	var ACTIVE_MODAL_CLASS_NAME = 'active2';
+	var openModalButton2 = document.querySelector('.open-modal-button2');
+	var closeModalButton = document.querySelector('.close-modal-button2');
+	var modalOverlay2 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME2);
+	openModalButton2.addEventListener('click',function(event){
+			modalOverlay2.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay2.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay2.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME2))
+				modalOverlay2.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME = 'modal-overlay';
+	var ACTIVE_MODAL_CLASS_NAME = 'active';
+	var openModalButton = document.querySelector('.open-modal-button');
+	var closeModalButton = document.querySelector('.close-modal-button');
+	var modalOverlay = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME);
+	openModalButton.addEventListener('click',function(event){
+			modalOverlay.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME))
+				modalOverlay.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+
+	var MODAL_OVERLAY_CLASS_NAME3 = 'modal-overlay3';
+	var ACTIVE_MODAL_CLASS_NAME = 'active3';
+	var openModalButton3 = document.querySelector('.open-modal-button3');
+	var closeModalButton = document.querySelector('.close-modal-button3');
+	var modalOverlay3 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME3);
+	openModalButton3.addEventListener('click',function(event){
+			modalOverlay3.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay3.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay3.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME3))
+				modalOverlay3.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME4 = 'modal-overlay4';
+	var ACTIVE_MODAL_CLASS_NAME = 'active4';
+	var openModalButton4 = document.querySelector('.open-modal-button4');
+	var closeModalButton = document.querySelector('.close-modal-button4');
+	var modalOverlay4 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME4);
+	openModalButton4.addEventListener('click',function(event){
+			modalOverlay4.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay4.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay4.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME4))
+				modalOverlay4.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME5 = 'modal-overlay5';
+	var ACTIVE_MODAL_CLASS_NAME = 'active5';
+	var openModalButton5 = document.querySelector('.open-modal-button5');
+	var closeModalButton = document.querySelector('.close-modal-button5');
+	var modalOverlay5 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME5);
+	openModalButton5.addEventListener('click',function(event){
+			modalOverlay5.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay5.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay5.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME5))
+				modalOverlay5.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME6 = 'modal-overlay6';
+	var ACTIVE_MODAL_CLASS_NAME = 'active6';
+	var openModalButton6 = document.querySelector('.open-modal-button6');
+	var closeModalButton = document.querySelector('.close-modal-button6');
+	var modalOverlay6 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME6);
+	openModalButton6.addEventListener('click',function(event){
+			modalOverlay6.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay6.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay6.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME6))
+				modalOverlay6.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME7 = 'modal-overlay7';
+	var ACTIVE_MODAL_CLASS_NAME = 'active7';
+	var openModalButton7 = document.querySelector('.open-modal-button7');
+	var closeModalButton = document.querySelector('.close-modal-button7');
+	var modalOverlay7 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME7);
+	openModalButton7.addEventListener('click',function(event){
+			modalOverlay7.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay7.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay7.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME7))
+				modalOverlay7.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME8 = 'modal-overlay8';
+	var ACTIVE_MODAL_CLASS_NAME = 'active8';
+	var openModalButton8 = document.querySelector('.open-modal-button8');
+	var closeModalButton = document.querySelector('.close-modal-button8');
+	var modalOverlay8 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME8);
+	openModalButton8.addEventListener('click',function(event){
+			modalOverlay8.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay8.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay8.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME8))
+				modalOverlay8.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME9 = 'modal-overlay9';
+	var ACTIVE_MODAL_CLASS_NAME = 'active9';
+	var openModalButton9 = document.querySelector('.open-modal-button9');
+	var closeModalButton = document.querySelector('.close-modal-button9');
+	var modalOverlay9 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME9);
+	openModalButton9.addEventListener('click',function(event){
+			modalOverlay9.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay9.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay9.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME9))
+				modalOverlay9.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME10 = 'modal-overlay10';
+	var ACTIVE_MODAL_CLASS_NAME = 'active10';
+	var openModalButton10 = document.querySelector('.open-modal-button10');
+	var closeModalButton = document.querySelector('.close-modal-button10');
+	var modalOverlay10 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME10);
+	openModalButton10.addEventListener('click',function(event){
+			modalOverlay10.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay10.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay10.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME10))
+				modalOverlay10.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME11 = 'modal-overlay11';
+	var ACTIVE_MODAL_CLASS_NAME = 'active11';
+	var openModalButton11 = document.querySelector('.open-modal-button11');
+	var closeModalButton = document.querySelector('.close-modal-button11');
+	var modalOverlay11 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME11);
+	openModalButton11.addEventListener('click',function(event){
+			modalOverlay11.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay11.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay11.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME11))
+				modalOverlay11.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME12 = 'modal-overlay12';
+	var ACTIVE_MODAL_CLASS_NAME = 'active12';
+	var openModalButton12 = document.querySelector('.open-modal-button12');
+	var closeModalButton = document.querySelector('.close-modal-button12');
+	var modalOverlay12 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME12);
+	openModalButton12.addEventListener('click',function(event){
+			modalOverlay12.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay12.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay12.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME12))
+				modalOverlay12.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME13 = 'modal-overlay13';
+	var ACTIVE_MODAL_CLASS_NAME = 'active13';
+	var openModalButton13 = document.querySelector('.open-modal-button13');
+	var closeModalButton = document.querySelector('.close-modal-button13');
+	var modalOverlay13 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME13);
+	openModalButton13.addEventListener('click',function(event){
+			modalOverlay13.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay13.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay13.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME13))
+				modalOverlay13.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME14 = 'modal-overlay14';
+	var ACTIVE_MODAL_CLASS_NAME = 'active14';
+	var openModalButton14 = document.querySelector('.open-modal-button14');
+	var closeModalButton = document.querySelector('.close-modal-button14');
+	var modalOverlay14 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME14);
+	openModalButton14.addEventListener('click',function(event){
+			modalOverlay14.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay14.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay14.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME14))
+				modalOverlay14.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME15 = 'modal-overlay15';
+	var ACTIVE_MODAL_CLASS_NAME = 'active15';
+	var openModalButton15 = document.querySelector('.open-modal-button15');
+	var closeModalButton = document.querySelector('.close-modal-button15');
+	var modalOverlay15 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME15);
+	openModalButton15.addEventListener('click',function(event){
+			modalOverlay15.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay15.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay15.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME15))
+				modalOverlay15.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME16 = 'modal-overlay16';
+	var ACTIVE_MODAL_CLASS_NAME = 'active16';
+	var openModalButton16 = document.querySelector('.open-modal-button16');
+	var closeModalButton = document.querySelector('.close-modal-button16');
+	var modalOverlay16 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME16);
+	openModalButton16.addEventListener('click',function(event){
+			modalOverlay16.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay16.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay16.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME16))
+				modalOverlay16.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME17 = 'modal-overlay17';
+	var ACTIVE_MODAL_CLASS_NAME = 'active17';
+	var openModalButton17 = document.querySelector('.open-modal-button17');
+	var closeModalButton = document.querySelector('.close-modal-button17');
+	var modalOverlay17 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME17);
+	openModalButton17.addEventListener('click',function(event){
+			modalOverlay17.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay17.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay17.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME17))
+				modalOverlay17.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME18 = 'modal-overlay18';
+	var ACTIVE_MODAL_CLASS_NAME = 'active18';
+	var openModalButton18 = document.querySelector('.open-modal-button18');
+	var closeModalButton = document.querySelector('.close-modal-button18');
+	var modalOverlay18 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME18);
+	openModalButton18.addEventListener('click',function(event){
+			modalOverlay18.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay18.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay18.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME18))
+				modalOverlay18.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME19 = 'modal-overlay19';
+	var ACTIVE_MODAL_CLASS_NAME = 'active19';
+	var openModalButton19 = document.querySelector('.open-modal-button19');
+	var closeModalButton = document.querySelector('.close-modal-button19');
+	var modalOverlay19 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME19);
+	openModalButton19.addEventListener('click',function(event){
+			modalOverlay19.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay19.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay19.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME19))
+				modalOverlay19.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+	
+	
+	var MODAL_OVERLAY_CLASS_NAME20 = 'modal-overlay20';
+	var ACTIVE_MODAL_CLASS_NAME = 'active20';
+	var openModalButton20 = document.querySelector('.open-modal-button20');
+	var closeModalButton = document.querySelector('.close-modal-button20');
+	var modalOverlay20 = document.querySelector('.'+ MODAL_OVERLAY_CLASS_NAME20);
+	openModalButton20.addEventListener('click',function(event){
+			modalOverlay20.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+		
+	});
+	
+	closeModalButton.addEventListener('click',function(event){
+		modalOverlay20.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	});
+	
+	modalOverlay20.addEventListener('click',function(event){
+		if (event.target.classList.contains(MODAL_OVERLAY_CLASS_NAME20))
+				modalOverlay20.classList.toggle(ACTIVE_MODAL_CLASS_NAME);
+	})
+};
+
+
+
+
